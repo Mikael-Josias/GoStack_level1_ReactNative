83552c43790a9711009f94c6f179994e663eae17
Mikael Josias <mikaeljosias2002@gmail.com>
2020-11-28 17:02:59 -0300

    style(index): style the components of the first page

--- GoStack_level1_ReactNative/mobile/src/index.js
+++ GoStack_level1_ReactNative/mobile/src/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+
+//Não possuem valor semântico (significado)
+//Não possuem estilização própria
+//Todos os componentes possuem por padrão "display: flex"
+
+//VIEW: div, footer, header, main, aside, section
+//TEXT: p, spam, strong, h1, h2, h3...
 
 
 export default function App() {
-    return <View></View>;
+    return (
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+            <View style={styles.container}>
+                <Text style={styles.title}>Hello GoStack!</Text>
+            </View>
+        </>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#7159c1",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    title: {
+        color: "#fff",
+        fontSize: 20,
+        fontWeight: "bold",
+    }
+});
